f530438f162e6e44cb1ede4feaa3884ec02a374b
jesus <jesusjsg@proton.me>
2025-02-11 07:53:10 -0400

    feat: Autotag and AutoIndent added

--- lua/gsus/plugins/treesitter.lua
+++ lua/gsus/plugins/treesitter.lua
@@ -1,47 +1,20 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	version = false,
+    event = { "BufReadPre", "BufNewFile" },
 	build = ":TSUpdate",
-	dependencies = {
-		"windwp/nvim-ts-autotag"
-	},
-	lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
-	init = function(plugin)
-		require("lazy.core.loader").add_to_rtp(plugin)
-		require("nvim-treesitter.query_predicates")
-	end,
-	keys = {
-		{ "<c-space>", desc = "Increment selection" },
-		{ "<bs>", desc = "Decrement selection", mode = "x" },
-	},
-	opts_extend = { "ensure_installed" },
-	opts = {
-		autotag = { enable = true },
-		highlight = { enable = true },
-		endsure_installed = {},
-		incremental_selection = {
-			enable = true,
-			keymaps = {
-				init_selection = "<C-space>",
-				node_incremental = "<C-space>",
-				scope_incremental = false,
-				node_decremental = "<bs>",
-			},
-		},
-		textobjects = {
-      			move = {
-        			enable = true,
-        			goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
-        			goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
-        			goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
-        			goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
-      			},
-    		},
-	},
-	config = function(_, opts)
-		if type(opts.ensure_installed) == "table" then
-			opts.ensure_installed = LazyVim.dedup(opts.ensure_installed)
-		end
-		require("nvim-treesitter.configs").setup(opts)
-	end,
+    dependecies = {
+        "windwp/nvim-ts-autotag",
+    },
+    config = function()
+        local treesitter = require("nvim-treesitter.configs")
+        
+        treesitter.setup({
+            ensure_installed = { "php", "javascript", "html", "twig", "css", "typescript", "markdown", "vim", "lua" },
+            sync_install = false,
+            auto_install = true,
+            highlight = { enable = true },
+            autotag = { enable = true },
+            indent = { enable = true },
+        })
+    end
 }
