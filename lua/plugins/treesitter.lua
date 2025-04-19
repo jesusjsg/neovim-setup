2d8d1ce795b630df8c48bd571cb1f9b296b02733
jesus <jesusjsg@proton.me>
2025-04-19 09:41:40 -0400

    feat: update lazy-lock and indent enabl

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,40 @@
 return {
     'nvim-treesitter/nvim-treesitter',
-    config = function()
-        require('nvim-treesitter').setup({
-            ensure_installed = { 'lua', 'python', 'javascript' },
-            sync_install = true,
-            auto_install = false,
-            highlight = { enable = true },
-            indent = { enable = true },
-            addittional_vim_regex_highlighting = false,
-        }) 
+    version = false,
+    build = ':TSUpdate',
+    event = { 'VeryLazy' },
+    lazy = vim.fn.argc(-1) == 0,
+    init = function(plugin)
+        require('lazy.core.loader').add_to_rtp(plugin)
+        require('nvim-treesitter.query_predicates')
+    end,
+    cmd = { 'TSUpdateSync', 'TSUpdate', 'TSInstall' },
+    opts_extend = { 'ensure_installed' },
+    opts = {
+        highlight = { enable = true },
+        indent = { enable = true },
+        ensure_installed = { 'lua', 'python', 'javascript', 'php' },
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = "<C-space>",
+                node_incremental = "<C-space>",
+                scope_incremental = false,
+                node_decremental = "<bs>",
+            },
+        },
+        textobjects = {
+            move = {
+                enable = true,
+                goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
+                goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
+                goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
+                goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
+            },
+        },
+
+    },
+    config = function(_, opts)
+        require('nvim-treesitter.configs').setup(opts)
     end
 }
