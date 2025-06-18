b0857ef1323045cb70ae9b54ad5f1047a015269b
jesusjsg <jesusjsgdev@gmail.com>
2025-06-18 00:05:42 +0000

    perf: conflicts fixed

--- lua/sortbito/plugins/colorscheme.lua
+++ lua/sortbito/plugins/colorscheme.lua
@@ -7,12 +7,4 @@ return {
 		require('github-theme').setup({})
 		vim.cmd('colorscheme github_dark_high_contrast')
 	end
-    'projekt0n/github-nvim-theme',
-    name = 'github-theme',
-    lazy = false,
-    priority = 1000,
-    config = function()
-        require('github-theme').setup({})
-        vim.cmd('colorscheme github_dark_high_contrast')
-    end
 }
--- lua/sortbito/plugins/nvim-tree.lua
+++ lua/sortbito/plugins/nvim-tree.lua
@@ -5,10 +5,6 @@ return {
         'nvim-tree/nvim-web-devicons',
     },
     config = function()
-	    vim.g.loaded_netwr = 1
-	    vim.g.loaded_netwrPlugin = 1
-
-	    require('nvim-tree').setup({
         vim.g.loaded_netwr = 1
         vim.g.loaded_netwrPlugin = 1
 
@@ -17,14 +13,10 @@ return {
             renderer = { group_empty = true },
             filters = { dotfiles = true }
         })
-        
         local keymap = vim.keymap.set
 
         keymap('n', '<leader>e', '<cmd>NvimTreeOpen<CR>', { desc = 'Open the tree' })
         keymap('n', '<leader>ee', '<cmd>NvimTreeToggle<CR>', { desc = 'Toggle file explorer' })
-
-        local keymap = vim.keymap.set
-
         keymap('n', '<leader>ee', '<cmd>NvimTreeOpen<CR>', { desc = 'Open the tree' })
         keymap('n', '<leader>ef', '<cmd>NvimTreeToggle<CR>', { desc = 'Toggle file explorer' })
         keymap('n', '<leader>ew', '<cmd>NvimTreeClose<CR>', { desc = 'Close the tree' })
