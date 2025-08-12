6dc7b04a615ef888bd20f9b200f6d6b06085f5eb
jesusjsg <jesusjsgdev@gmail.com>
2025-08-12 01:52:57 +0000

    feat: trouble plugin added, miss the keymaps

--- lua/sortbito/plugins/lsp/lsp.lua
+++ lua/sortbito/plugins/lsp/lsp.lua
@@ -7,7 +7,6 @@ return {
 
         vim.diagnostic.config({
             virtual_text = true,
-            signs = true,
             underline = true,
             update_in_insert = false,
             severity_sort = true
