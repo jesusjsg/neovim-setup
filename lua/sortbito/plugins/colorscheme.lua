5c66a0f7293d53aea651b3568d85114e563362aa
jesusjsg <jesusjsgdev@gmail.com>
2025-08-13 02:27:26 +0000

    feat: new colorscheme tokyonight with dark bg and setting nvim-tree colors

--- lua/sortbito/plugins/colorscheme.lua
+++ lua/sortbito/plugins/colorscheme.lua
@@ -1,7 +1,20 @@
 return {
-    "rose-pine/neovim",
-    name = "rose-pine",
+    "folke/tokyonight.nvim",
+    style = "night",
     config = function()
-        vim.cmd("colorscheme rose-pine")
+        require("tokyonight").setup({
+            style = "night",
+            on_colors = function(colors)
+                colors.bg = "#000000"
+            end,
+            on_highlights = function(hl, c)
+                hl.Normal = { bg = c.bg, fg = c.fg }
+
+                hl.NvimTreeNormal = { bg = c.bg, fg = c.fg }
+                hl.NvimTreeNormalNC = { bg = c.bg, fg = c.fg }
+                hl.NvimTreeEndOfBuffer = { bg = c.bg, fg = c.fg }
+            end
+        })
+        vim.cmd("colorscheme tokyonight-night")
     end
 }
