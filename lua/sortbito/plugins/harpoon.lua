5d2f9cfdeed9d3e548353c6cbe12c6c353a3bfb9
jesusjsg <jesusjsgdev@gmail.com>
2025-09-06 16:00:52 +0000

    feat: add more key mappings to harpoon
    This commit adds key mappings for Ctrl-k, Ctrl-l, Ctrl-u and Ctrl-i to select the 3rd, 4th, 5th and 6th file in the harpoon list.

--- lua/sortbito/plugins/harpoon.lua
+++ lua/sortbito/plugins/harpoon.lua
@@ -11,5 +11,9 @@ return {
         keymap('n', '<C-e>', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
         keymap('n', '<C-h>', function() harpoon:list():select(1) end)
         keymap('n', '<C-j>', function() harpoon:list():select(2) end)
+        keymap('n', '<C-k>', function() harpoon:list():select(3) end)
+        keymap('n', '<C-l>', function() harpoon:list():select(4) end)
+        keymap('n', '<C-u>', function() harpoon:list():select(5) end)
+        keymap('n', '<C-i>', function() harpoon:list():select(6) end)
     end
 }
