5675537546c593ca06bb889ae807fce02e4aafea
jesus <jesusjsgdev@gmail.com>
2025-11-11 19:14:37 -0400

    fix(colorscheme): remove unnecessary config

--- lua/sortbito/plugins/colorscheme.lua
+++ lua/sortbito/plugins/colorscheme.lua
@@ -4,16 +4,6 @@ return {
     config = function()
         require("tokyonight").setup({
             style = "night",
-            on_colors = function(colors)
-                colors.bg = "#000000"
-            end,
-            on_highlights = function(hl, c)
-                hl.Normal = { bg = c.bg, fg = c.fg }
-
-                hl.NvimTreeNormal = { bg = c.bg, fg = c.fg }
-                hl.NvimTreeNormalNC = { bg = c.bg, fg = c.fg }
-                hl.NvimTreeEndOfBuffer = { bg = c.bg, fg = c.fg }
-            end
         })
         vim.cmd("colorscheme tokyonight-night")
     end
