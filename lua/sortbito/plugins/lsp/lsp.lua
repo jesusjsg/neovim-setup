0db58bed176cff48caf0d446cd3c2f824f49effc
jesus <jesusjsgdev@gmail.com>
2025-12-30 14:55:43 -0400

    feat: LSP config deprecated was remove

--- lua/sortbito/plugins/lsp/lsp.lua
+++ lua/sortbito/plugins/lsp/lsp.lua
@@ -1,28 +1,4 @@
 return {
-    'neovim/nvim-lspconfig',
-    config = function()
-        local lspconfig = require('lspconfig')
-        local mason_lspconfig = require('mason-lspconfig')
-        local cmp_nvim_lsp = require('cmp_nvim_lsp')
-
-        vim.diagnostic.config({
-            virtual_text = true,
-            underline = true,
-            update_in_insert = false,
-            severity_sort = true
-        })
-
-        local capabilities = cmp_nvim_lsp.default_capabilities()
-
-        mason_lspconfig.setup({
-            ensure_installed = { 'lua_ls', 'ty', 'ts_ls' },
-            handlers = {
-                function(server_name)
-                    lspconfig[server_name].setup({
-                        capabilities = capabilities
-                    })
-                end
-            }
-        })
-    end
+    vim.lsp.enable("ty"),
+    vim.lsp.enable("lua_ls")
 }
