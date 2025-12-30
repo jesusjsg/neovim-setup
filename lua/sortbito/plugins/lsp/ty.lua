b061dc7a76049b0de50e1e031d64677eda6c88ac
jesus <jesusjsgdev@gmail.com>
2025-12-30 14:56:17 -0400

    feat: New individual config of each lsp

---
+++ lua/sortbito/plugins/lsp/ty.lua
@@ -0,0 +1,7 @@
+return {
+    vim.lsp.config("ty", {
+        cmd = { "ty", "server" },
+        filetypes = { "python" },
+        root_markers = { "ty.toml", "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", ".git" }
+    })
+}
