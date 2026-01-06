0cb7686ef0af813be6f1534b029655a2f3f85ff0
jesus <jesusjsgdev@gmail.com>
2026-01-06 16:56:15 -0400

    style: reformat files with stylua

--- lua/sortbito/plugins/treesitter.lua
+++ lua/sortbito/plugins/treesitter.lua
@@ -1,14 +1,14 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    tag = 'v0.10.0',
-    lazy = false,
-    branch = 'main',
-    build = ':TSUpdate',
-    config = function()
-        require('nvim-treesitter.configs').setup({
-            ensure_installed = { "lua", "markdown", "python", "javascript", "html", "css" },
-            highlight = { enable = true },
-            --indent = { enable = true },
-        })
-    end
+  "nvim-treesitter/nvim-treesitter",
+  tag = "v0.10.0",
+  lazy = false,
+  branch = "main",
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = { "lua", "markdown", "python", "javascript" },
+      highlight = { enable = true },
+      --indent = { enable = true },
+    })
+  end,
 }
