b568edbe24ef4ad8930d15e42bae20272f8f4852
jesus <jesusjsgdev@gmail.com>
2026-01-06 22:45:38 -0400

    feat: keymaps added and removed to use another keybidings

--- lua/sortbito/plugins/harpoon.lua
+++ lua/sortbito/plugins/harpoon.lua
@@ -1,19 +1,32 @@
 return {
-    'ThePrimeagen/harpoon',
-    branch = 'harpoon2',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-        local harpoon = require('harpoon')
-        local keymap = vim.keymap.set
-        harpoon:setup()
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local harpoon = require("harpoon")
+    local keymap = vim.keymap.set
+    harpoon:setup()
 
-        keymap('n', '<leader>rr', function() harpoon:list():add() end)
-        keymap('n', '<C-e>', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-        keymap('n', '<C-h>', function() harpoon:list():select(1) end)
-        keymap('n', '<C-j>', function() harpoon:list():select(2) end)
-        keymap('n', '<C-k>', function() harpoon:list():select(3) end)
-        keymap('n', '<C-l>', function() harpoon:list():select(4) end)
-        keymap('n', '<C-u>', function() harpoon:list():select(5) end)
-        keymap('n', '<C-i>', function() harpoon:list():select(6) end)
-    end
+    keymap("n", "<leader>rr", function()
+      harpoon:list():add()
+    end)
+    keymap("n", "<C-e>", function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end)
+    keymap("n", "<C-h>", function()
+      harpoon:list():select(1)
+    end)
+    keymap("n", "<C-j>", function()
+      harpoon:list():select(2)
+    end)
+    keymap("n", "<C-k>", function()
+      harpoon:list():select(3)
+    end)
+    keymap("n", "<C-l>", function()
+      harpoon:list():select(4)
+    end)
+    keymap("n", "<C-u>", function()
+      harpoon:list():select(5)
+    end)
+  end,
 }
--- lua/sortbito/plugins/nvim-tree.lua
+++ lua/sortbito/plugins/nvim-tree.lua
@@ -1,23 +1,27 @@
 return {
-    'nvim-tree/nvim-tree.lua',
-    lazy = false,
-    dependencies = {
-        'nvim-tree/nvim-web-devicons',
-    },
-    config = function()
-        vim.g.loaded_netwr = 1
-        vim.g.loaded_netwrPlugin = 1
+  "nvim-tree/nvim-tree.lua",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    vim.g.loaded_netwr = 1
+    vim.g.loaded_netwrPlugin = 1
 
-        require('nvim-tree').setup({
-            sort = { sorter = 'case_sensitive' },
-            renderer = { group_empty = true },
-            filters = { dotfiles = true }
-        })
-        local keymap = vim.keymap.set
+    require("nvim-tree").setup({
+      sort = { sorter = "case_sensitive" },
+      renderer = { group_empty = true },
+      filters = { dotfiles = true },
+    })
+    local keymap = vim.keymap.set
 
-        keymap('n', '<leader>e', '<cmd>NvimTreeOpen<CR>', { desc = 'Open the tree' })
-        keymap('n', '<leader>ee', '<cmd>NvimTreeOpen<CR>', { desc = 'Open the tree' })
-        keymap('n', '<leader>ef', '<cmd>NvimTreeToggle<CR>', { desc = 'Toggle file explorer' })
-        keymap('n', '<leader>ew', '<cmd>NvimTreeClose<CR>', { desc = 'Close the tree' })
-    end
+    keymap("n", "<leader>ee", "<cmd>NvimTreeOpen<CR>", { desc = "Open the tree" })
+    keymap("n", "<leader>eq", "<cmd>NvimTreeClose<CR>", { desc = "Closes the tree" })
+    keymap(
+      "n",
+      "<leader>ef",
+      "<cmd>NvimTreeFindFile<CR>",
+      { desc = "Change the cursor in the tree for the current bufname" }
+    )
+  end,
 }
