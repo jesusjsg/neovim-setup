aab07e07bf619b9c2fbd268390c91c0ad824b703
jesus <jesusjsgdev@gmail.com>
2026-01-06 22:44:29 -0400

    style: conform file formated with stylua

--- lua/sortbito/plugins/conform.lua
+++ lua/sortbito/plugins/conform.lua
@@ -1,22 +1,21 @@
 return {
-    'stevearc/conform.nvim',
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-        require('conform').setup({
-            formatters_by_ft = {
-                javascript = { "prettier" },
-                typescript = { "prettier" },
-                typescriptreact = { "prettier" },
-                python = { "ruff_format", "ruff_fix" },
-                lua = { "stylua" },
-                json = { "prettier" },
-                jsonc = { "prettier" }
-
-            },
-            format_on_save = {
-                timeout_ms = 500,
-                lsp_format = "fallback"
-            },
-        })
-    end
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    require("conform").setup({
+      formatters_by_ft = {
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        typescriptreact = { "prettier" },
+        python = { "ruff_format", "ruff_fix" },
+        lua = { "stylua" },
+        json = { "prettier" },
+        jsonc = { "prettier" },
+      },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_format = "fallback",
+      },
+    })
+  end,
 }
