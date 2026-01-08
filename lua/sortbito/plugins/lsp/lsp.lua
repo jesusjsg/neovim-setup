e1e1a11be7534fe1dd22119cda41f9eea5f01571
jesus <jesusjsgdev@gmail.com>
2026-01-07 21:21:56 -0400

    style: reformat lsp files with stylua

--- lua/sortbito/plugins/lsp/lsp.lua
+++ lua/sortbito/plugins/lsp/lsp.lua
@@ -1,4 +1,4 @@
 return {
-    vim.lsp.enable("ty"),
-    vim.lsp.enable("lua_ls")
+  vim.lsp.enable("ty"),
+  vim.lsp.enable("lua_ls"),
 }
--- lua/sortbito/plugins/lsp/luals.lua
+++ lua/sortbito/plugins/lsp/luals.lua
@@ -1,7 +1,17 @@
 return {
-    vim.lsp.config("lua_ls", {
-        cmd = { "lua-language-server" },
-        filetypes = { "lua" },
-        root_markers = { ".emmyrc.json", ".luarc.json", ".luarc.jsonc", ".luacheckrc", ".stylua.toml", "stylua.toml", "selene.toml", "selene.yml", ".git" }
-    })
+  vim.lsp.config("lua_ls", {
+    cmd = { "lua-language-server" },
+    filetypes = { "lua" },
+    root_markers = {
+      ".emmyrc.json",
+      ".luarc.json",
+      ".luarc.jsonc",
+      ".luacheckrc",
+      ".stylua.toml",
+      "stylua.toml",
+      "selene.toml",
+      "selene.yml",
+      ".git",
+    },
+  }),
 }
