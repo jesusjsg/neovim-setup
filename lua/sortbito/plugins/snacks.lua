853e7afb8539d392322674e2dd75a61ff2e1a082
jesus <jesusjsgdev@gmail.com>
2026-01-17 16:38:10 -0400

    feat(snacks): disable dashboard

--- lua/sortbito/plugins/snacks.lua
+++ lua/sortbito/plugins/snacks.lua
@@ -4,7 +4,6 @@ return {
   lazy = false,
   opts = {
     bigfile = { enabled = true },
-    dashboard = { enabled = true },
     indent = { enabled = true },
     input = { enabled = true },
     notifier = { enabled = true },
