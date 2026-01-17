4ae0a4df5ab7d5c9c694a35dac429101c8ec860f
jesus <jesusjsgdev@gmail.com>
2026-01-17 18:46:13 -0400

    feat: add diagnostic configuration and keymaps

--- lua/sortbito/plugins/lsp/lsp.lua
+++ lua/sortbito/plugins/lsp/lsp.lua
@@ -1,8 +1,32 @@
 return {
   "mason-org/mason-lspconfig.nvim",
-  opts = { ensure_installed = { "lua_ls", "ty", "ruff" } },
+  opts = {
+    ensure_installed = { "lua_ls", "ty", "ruff" },
+  },
   dependencies = {
-    { "mason-org/mason.nvim", opts = { opts } },
+    { "mason-org/mason.nvim", opts = true },
     "neovim/nvim-lspconfig",
   },
+  config = function(_, opts)
+    require("mason").setup()
+    require("mason-lspconfig").setup(opts)
+
+    vim.diagnostic.config({
+      virtual_text = true,
+      signs = true,
+      underline = true,
+      update_in_insert = false,
+      severity_sort = true,
+      float = {
+        focusable = false,
+        border = "rounded",
+        source = "always",
+      },
+    })
+
+    vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic" })
+    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Prev diagnostic" })
+    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
+    vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Diagnostic list" }) -- In some times this keymap is slow
+  end,
 }
