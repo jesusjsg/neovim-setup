9bc40b3517ac1f07c102bfda47adcf4af0f46610
jesus <jesusjsgdev@gmail.com>
2026-01-17 01:51:35 -0400

    refactor(blink): miss supermaven autocomplete

--- lua/sortbito/plugins/blink.lua
+++ lua/sortbito/plugins/blink.lua
@@ -1,33 +1,59 @@
 return {
-  "saghen/blink.cmp",
-  dependencies = { "rafamadriz/friendly-snippets" },
-
-  opts = {
-    keymap = { preset = "enter" },
-
-    sources = {
-      default = { "lsp", "path", "snippets", "buffer" },
+  {
+    "saghen/blink.cmp",
+    version = "1.*",
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+      {
+        "supermaven-inc/supermaven-nvim",
+        opts = {
+          disable_inline_completion = false,
+          disable_keymaps = false,
+          keymaps = {
+            accept_suggestion = "<C-]>",
+            clear_suggestion = "<C-c>",
+            accept_word = "<C-[",
+          },
+        },
+      },
+      { "huijiro/blink-cmp-supermaven" },
     },
 
-    completion = {
-      accept = {
-        auto_brackets = {
-          enabled = true,
+    opts = {
+      keymap = { preset = "enter" },
+
+      appearance = {
+        use_nvim_cmp_as_default = true,
+        nerd_font_variant = "mono",
+      },
+
+      completion = {
+        accept = {
+          auto_brackets = {
+            enabled = true,
+          },
+        },
+      },
+
+      sources = {
+        default = { "lsp", "path", "supermaven", "snippets", "buffer" },
+        providers = {
+          supermaven = {
+            name = "supermaven",
+            module = "blink-cmp-supermaven",
+            async = true,
+          },
         },
       },
     },
-  },
-  opts_extend = { "sources.default" },
-  {
-    "supermaven-inc/supermaven-nvim",
-    config = function()
-      require("supermaven-nvim").setup({
-        keymaps = {
-          accept_suggestion = "<C-]>",
-          clear_suggestion = "<C-c>",
-          accept_word = "<C-[",
-        },
-      })
+    config = function(_, opts)
+      require("blink.cmp").setup(opts)
+
+      if vim.lsp.config then
+        vim.lsp.config("*", {
+          capabilities = require("blink.cmp").get_lsp_capabilities(),
+        })
+      end
     end,
   },
 }
