33ccccdf79f0fdde896c6078f009b07e7baf109f
jesus <jesusjsgdev@gmail.com>
2026-02-16 09:18:34 -0400

    feat: supermaven pluggin added to use AI autocompletion

--- lua/sortbito/plugins/blink.lua
+++ lua/sortbito/plugins/blink.lua
@@ -44,4 +44,13 @@ return {
       end
     end,
   },
+  {
+    "supermaven-inc/supermaven-nvim",
+    config = function()
+      require("supermaven-nvim").setup({
+        keymaps = { accept_suggestion = "<C-l>", accept_word = "<C-]>" },
+        disable_inline_completion = false,
+      })
+    end,
+  },
 }
